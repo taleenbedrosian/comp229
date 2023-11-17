55acd20646031cd155b27cc1430623d34740c9e8
Taleen Bedrosian <taleen.bedrosian@gmail.com>
2023-11-17 05:13:32 -0500

    modified

--- index.js
+++ index.js
@@ -69,29 +69,67 @@ app.get("/api/users", (req, res) => {
     })();
   });
 //get...fetch a user
-app.get("/api/users/:userid", (req, res) => {
-    (async () => {
-      try {
-        const reqDoc = db.collection("userdetails").doc(req.params.id);
-        let userDetail = await reqDoc.get();
-        let response = userDetail.data();
-  
-        return res.status(200).send({ status: "Success", data: response });
-      } catch (error) {
-        console.log(error);
-        res.status(500).send({ status: "Failed", msg: error });
-      }
-    })();
-  });
 
+app.get("/api/users/:userid", async (req, res) => {
+  try {
+    const reqDoc = db.collection("userdetails").doc(req.params.userid);
+    const userDetail = await reqDoc.get();
 
+    if (!userDetail.exists) {
+      return res.status(404).send({ status: "Not Found", msg: "User not found" });
+    }
 
+    const userData = userDetail.data();
+    const response = {
+      status: "Success",
+      data: {
+        id: userData.id,
+        email: userData.email,
+        name: userData.name,
+      },
+    };
 
+    return res.status(200).send(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: "Failed", msg: error.message });
+  }
+});
 
 
 
 
 //put...update a user
+
+app.put("/api/users/:userid", (req, res) => {
+  (async () => {
+    try {
+      const reqDoc = db.collection("userdetails").doc(req.params.userid);
+      await reqDoc.update({
+        name: req.body.name,
+        email: req.body.email,
+        merge: true 
+      });
+      return res.status(200).send({ status: "Success", msg: "Data Updated" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ status: "Failed", msg: error });
+    }
+  })();
+});
+
 //delete...delete a user
-//post...user sign in 
+app.delete("/api/users/:userid", (req, res) => {
+  (async () => {
+    try {
+      const reqDoc = db.collection("userdetails").doc(req.params.userid);
+      await reqDoc.delete();
+      return res.status(200).send({ status: "Success", msg: "Data Removed" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ status: "Failed", msg: error });
+    }
+  })();
+});
+
 exports.app = functions.https.onRequest(app);
